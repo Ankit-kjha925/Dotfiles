3cdd16f231e922abbf521b04d63805c12332f27a
Ankit-kjha925 <ankitkumarjha04@outlook.com>
2026-03-03 11:48:14 +0530

    add tmux config

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,16 +1,64 @@
-require "nvchad.mappings"
-
--- add yours here
 local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+-- SMART SPLIT NAVIGATION (Synced with tmux)
+map("n", "<C-h>", "<C-w>h", opts)
+map("n", "<C-j>", "<C-w>j", opts)
+map("n", "<C-k>", "<C-w>k", opts)
+map("n", "<C-l>", "<C-w>l", opts)
 
-map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- SAVE / QUIT
+map("n", "<leader>w", "<cmd>w<CR>", { desc = "Save file" })
+map("n", "<leader>q", "<cmd>q<CR>", { desc = "Quit window" })
+map("n", "<leader>Q", "<cmd>qa!<CR>", { desc = "Force quit all" })
 
--- tmux navigator
--- map("n", "<leader>h", "<cmd>TmuxNavigateLeft<CR>",  { desc = "Tmux left" })
--- map("n", "<leader>j", "<cmd>TmuxNavigateDown<CR>",  { desc = "Tmux down" })
--- map("n", "<leader>k", "<cmd>TmuxNavigateUp<CR>",    { desc = "Tmux up" })
--- map("n", "<leader>l", "<cmd>TmuxNavigateRight<CR>", { desc = "Tmux right" })
+-- BUFFER NAVIGATION
+map("n", "<S-l>", "<cmd>bnext<CR>", { desc = "Next buffer" })
+map("n", "<S-h>", "<cmd>bprevious<CR>", { desc = "Previous buffer" })
+map("n", "<leader>bd", "<cmd>bdelete<CR>", { desc = "Close buffer" })
 
+-- TELESCOPE (Primary Navigation Layer)
+local telescope = require("telescope.builtin")
+
+map("n", "<leader>ff", telescope.find_files, { desc = "Find files" })
+map("n", "<leader>fg", telescope.live_grep, { desc = "Live grep" })
+map("n", "<leader>fb", telescope.buffers, { desc = "Find buffers" })
+map("n", "<leader>fh", telescope.help_tags, { desc = "Help tags" })
+map("n", "<leader>fr", telescope.oldfiles, { desc = "Recent files" })
+
+-- LSP KEYBINDINGS
+map("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+map("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+map("n", "gr", vim.lsp.buf.references, { desc = "Find references" })
+map("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
+map("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation" })
+map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
+map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
+map("n", "<leader>f", function()
+  vim.lsp.buf.format({ async = true })
+end, { desc = "Format file" })
+
+-- GIT
+map("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<CR>", { desc = "Preview hunk" })
+map("n", "<leader>gr", "<cmd>Gitsigns reset_hunk<CR>", { desc = "Reset hunk" })
+map("n", "<leader>gb", "<cmd>Gitsigns blame_line<CR>", { desc = "Blame line" })
+map("n", "<leader>gd", "<cmd>Gitsigns diffthis<CR>", { desc = "Git diff" })
+
+-- TERMINAL
+map("n", "<leader>tt", "<cmd>ToggleTerm<CR>", { desc = "Toggle terminal" })
+map("t", "<Esc>", "<C-\\><C-n>", opts)
+
+-- MOVE LINES (VISUAL MODE)
+map("v", "J", ":m '>+1<CR>gv=gv", opts)
+map("v", "K", ":m '<-2<CR>gv=gv", opts)
+
+-- QUICK ESCAPE
+map("i", "jk", "<ESC>", opts)
+
+-- CENTER AFTER SEARCH
+map("n", "n", "nzzzv", opts)
+map("n", "N", "Nzzzv", opts)
+
+-- KEEP CURSOR CENTERED WHEN SCROLLING
+map("n", "<C-d>", "<C-d>zz", opts)
+map("n", "<C-u>", "<C-u>zz", opts)
