4ef9344c576c082ab65d0603346394bb6128faae
Ankit-kjha925 <ankitkumarjha04@outlook.com>
2026-03-03 17:10:43 +0530

    update dotfiles

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -3,21 +3,20 @@
 -- Please read that file to know all available options :( 
 
 ---@type ChadrcConfig
+---@type ChadrcConfig
 local M = {}
 
+-- Leader key must be defined before anything
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+---@diagnostic disable-next-line
 M.base46 = {
   theme = "default",
 }
 
 M.nvdash = {
-  load_on_startup = false, --changeto true if needed.
+  load_on_startup = false,
 }
 
---M.ui = {
-  --tabufline = {
-    --lazyload = false,
-  --},
---}
-
 return M
-
