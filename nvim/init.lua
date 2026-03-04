433f14e868163cf7c335f75cdfc1a4b4146050a1
Ankit-kjha925 <ankitkumarjha04@outlook.com>
2026-03-04 10:47:41 +0530

    added autocommands for nvchad added plugins, many plugins added mappings for those plugins updated init.lua file

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,21 +1,25 @@
+-- LEADER MUST BE FIRST
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 
--- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-
 if not vim.uv.fs_stat(lazypath) then
-  local repo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+  vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  }
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 local lazy_config = require "configs.lazy"
 
--- load plugins
 require("lazy").setup({
   {
     "NvChad/NvChad",
@@ -27,13 +31,9 @@ require("lazy").setup({
   { import = "plugins" },
 }, lazy_config)
 
--- load theme
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
 
 require "options"
 require "autocmds"
-
-vim.schedule(function()
-  require "mappings"
-end)
+require "mappings"
