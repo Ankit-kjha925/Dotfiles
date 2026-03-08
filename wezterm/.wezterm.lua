bc8a56d411af09fc880f1b17fcc13edb11fdcae5
Ankit-kjha925 <ankitkumarjha04@outlook.com>
2026-03-08 16:38:26 +0530

    update dotfiles

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,32 +1,69 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
-
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
+-- General
+config.font_size = 15
+config.line_height = 1
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.window_background_opacity = 0.75
+config.macos_window_background_blur = 15
 
 config.colors = {
-	foreground = "#CBE0F0",
-	background = "#011423",
-	cursor_bg = "#47FF9C",
-	cursor_border = "#47FF9C",
-	cursor_fg = "#011423",
-	selection_bg = "#033259",
-	selection_fg = "#CBE0F0",
-	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+  cursor_bg = "#7aa2f7",
+  cursor_border = "#7aa2f7",
 }
 
-config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 16
-
-config.enable_tab_bar = false
+config.window_padding = {
+  left = 12,
+  right = 12,
+  top = 12,
+  bottom = 12,
+}
 
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 10
+config.enable_tab_bar = false
 
--- and finally, return the configuration to wezterm
+-- Disable close confirmation
+config.window_close_confirmation = "NeverPrompt"
+
+-- Key Bindings
+config.keys = {
+  {
+    key = "w",
+    mods = "CMD",
+    action = wezterm.action.CloseCurrentPane { confirm = false },
+  },
+  {
+    key = "d",
+    mods = "CMD",
+    action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" },
+  },
+  {
+    key = "d",
+    mods = "CMD|SHIFT",
+    action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" },
+  },
+  -- Pane Navigation
+  {
+    key = "LeftArrow",
+    mods = "CMD",
+    action = wezterm.action.ActivatePaneDirection "Left",
+  },
+  {
+    key = "RightArrow",
+    mods = "CMD",
+    action = wezterm.action.ActivatePaneDirection "Right",
+  },
+  {
+    key = "UpArrow",
+    mods = "CMD",
+    action = wezterm.action.ActivatePaneDirection "Up",
+  },
+  {
+    key = "DownArrow",
+    mods = "CMD",
+    action = wezterm.action.ActivatePaneDirection "Down",
+  },
+}
+
 return config
-
